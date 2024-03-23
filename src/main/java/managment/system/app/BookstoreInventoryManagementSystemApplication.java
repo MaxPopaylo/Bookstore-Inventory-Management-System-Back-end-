56308729ce4f3c36d773f7dadb4e74f01f500823
MaxPopaylo <max.popaylo@gmail.com>
2024-03-23 10:45:19 +0200

    Initialize the project

---
+++ src/main/java/managment/system/app/BookstoreInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package managment.system.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreInventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/managment/system/app/BookstoreInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package managment.system.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
